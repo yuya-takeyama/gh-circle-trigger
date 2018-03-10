d2b4a5108ff21c6f113939b8b42c99b85e0b65fd
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2018-03-11 00:15:51 +0900

    TypeScriptify

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./build/index').default;
-
-const port = parseInt(
-  typeof process.env.PORT === 'string' ? process.env.PORT : '3000',
-  10,
-);
-
-app.listen(port);
-console.log(`Listening on ${port}...`);
