7e04a151152f6eea68d051d2c29b8f07e70b6e45
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2018-03-11 05:25:37 +0900

    Add tests

--- src/Handler.test.ts
+++ src/Handler.test.ts
@@ -50,69 +50,87 @@ describe('handler', () => {
     };
 
     describe('when it contains the trigger word', () => {
-      beforeEach(() => {
-        event.payload.pull_request.body = '@bot trigger the_job';
-      });
-
-      describe('and it succeeded to trigger the job', () => {
+      describe('trigger command', () => {
         beforeEach(() => {
-          moxios.stubRequest(
-            'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
-            {
-              status: 201,
-              response: { build_url: 'https://circleci.com/build_url' },
-            },
-          );
-          moxios.stubRequest(
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            { status: 201, response: {} },
-          );
+          event.payload.pull_request.body = '@bot trigger the_job';
         });
 
-        it('returns the triggered job', async () => {
-          const result = await handler.handle(event, []);
-
-          expect(result).toEqual('Trigger: the_job, Branch: fix');
-        });
-
-        describe('when allowedJobs is not empty', () => {
-          describe('and the job is in allowedJobs', async () => {
-            it('returns the triggered job', async () => {
-              const result = await handler.handle(event, ['the_job']);
-
-              expect(result).toEqual('Trigger: the_job, Branch: fix');
-            });
+        describe('and it succeeded to trigger the job', () => {
+          beforeEach(() => {
+            moxios.stubRequest(
+              'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
+              {
+                status: 201,
+                response: { build_url: 'https://circleci.com/build_url' },
+              },
+            );
+            moxios.stubRequest(
+              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              { status: 201, response: {} },
+            );
           });
 
-          describe('and the job is not in allowedJobs', async () => {
-            it('returns the triggered job', async () => {
-              const result = await handler.handle(event, ['not_the_job']);
+          it('returns the triggered job', async () => {
+            const result = await handler.handle(event, []);
 
-              expect(result).toEqual('Not allowed: the_job');
+            expect(result).toEqual('Trigger: the_job, Branch: fix');
+          });
+
+          describe('when allowedJobs is not empty', () => {
+            describe('and the job is in allowedJobs', async () => {
+              it('returns the triggered job', async () => {
+                const result = await handler.handle(event, ['the_job']);
+
+                expect(result).toEqual('Trigger: the_job, Branch: fix');
+              });
             });
+
+            describe('and the job is not in allowedJobs', async () => {
+              it('returns the triggered job', async () => {
+                const result = await handler.handle(event, ['not_the_job']);
+
+                expect(result).toEqual('Not allowed: the_job');
+              });
+            });
+          });
+        });
+
+        describe('and it failed to trigger the job', () => {
+          beforeEach(() => {
+            moxios.stubRequest(
+              'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
+              {
+                status: 403,
+                response: { message: 'Forbidden' },
+              },
+            );
+            moxios.stubRequest(
+              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              { status: 201, response: {} },
+            );
+          });
+
+          it('throws an error', async () => {
+            const err = await handler.handle(event, []).catch(err => err);
+
+            expect(err.message).toEqual('Request failed with status code 403');
           });
         });
       });
 
-      describe('and it failed to trigger the job', () => {
+      describe('help command', () => {
         beforeEach(() => {
-          moxios.stubRequest(
-            'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
-            {
-              status: 403,
-              response: { message: 'Forbidden' },
-            },
-          );
-          moxios.stubRequest(
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            { status: 201, response: {} },
-          );
+          event.payload.pull_request.body = '@bot help';
         });
 
-        it('throws an error', async () => {
-          const err = await handler.handle(event, []).catch(err => err);
+        it('returns "Help"', async () => {
+          moxios.stubRequest(event.payload.pull_request.comments_url, {
+            status: 201,
+            response: {},
+          });
+          const result = await handler.handle(event, ['allowed_job']);
 
-          expect(err.message).toEqual('Request failed with status code 403');
+          expect(result).toEqual('Help');
         });
       });
     });
@@ -125,38 +143,42 @@ describe('handler', () => {
   });
 
   describe("when it's IssueCommentEvent", () => {
-    const event: IssueCommentEvent = {
-      event: 'issue_comment',
-      payload: {
-        action: 'created',
-        issue: {
-          url:
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2',
-          comments_url:
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-          pull_request: {
+    let event: IssueCommentEvent;
+
+    beforeEach(() => {
+      event = {
+        event: 'issue_comment',
+        payload: {
+          action: 'created',
+          issue: {
             url:
-              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
-            id: 172665130,
-            body: 'Hello',
+              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2',
             comments_url:
               'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            head: {
-              ref: 'fix',
-            },
-            base: {
-              ref: 'master',
+            pull_request: {
+              url:
+                'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
+              id: 172665130,
+              body: 'Hello',
+              comments_url:
+                'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              head: {
+                ref: 'fix',
+              },
+              base: {
+                ref: 'master',
+              },
             },
           },
+          comment: {
+            body: 'Hello',
+          },
+          repository: {
+            full_name: 'yuya-takeyama/gh-circle-trigger-proto',
+          },
         },
-        comment: {
-          body: 'Hello',
-        },
-        repository: {
-          full_name: 'yuya-takeyama/gh-circle-trigger-proto',
-        },
-      },
-    };
+      };
+    });
 
     describe('when it contains the trigger word', () => {
       beforeEach(() => {
@@ -181,73 +203,93 @@ describe('handler', () => {
         });
       });
 
-      describe('and it succeeded to trigger the job', () => {
-        beforeEach(() => {
-          moxios.stubRequest(
-            'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
-            {
-              status: 201,
-              response: { build_url: 'https://circleci.com/build_url' },
-            },
-          );
-          moxios.stubRequest(
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            { status: 201, response: {} },
-          );
-        });
-
-        it('returns the triggered job', async () => {
-          const result = await handler.handle(event, []);
-
-          expect(result).toEqual('Trigger: the_job, Branch: fix');
-        });
-
-        describe('when allowedJobs is not empty', () => {
-          describe('and the job is in allowedJobs', async () => {
-            it('returns the triggered job', async () => {
-              const result = await handler.handle(event, ['the_job']);
-
-              expect(result).toEqual('Trigger: the_job, Branch: fix');
-            });
+      describe('trigger command', () => {
+        describe('and it succeeded to trigger the job', () => {
+          beforeEach(() => {
+            moxios.stubRequest(
+              'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
+              {
+                status: 201,
+                response: { build_url: 'https://circleci.com/build_url' },
+              },
+            );
+            moxios.stubRequest(
+              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              { status: 201, response: {} },
+            );
           });
 
-          describe('and the job is not in allowedJobs', async () => {
-            it('returns the triggered job', async () => {
-              const result = await handler.handle(event, ['not_the_job']);
+          it('returns the triggered job', async () => {
+            const result = await handler.handle(event, []);
 
-              expect(result).toEqual('Not allowed: the_job');
+            expect(result).toEqual('Trigger: the_job, Branch: fix');
+          });
+
+          describe('when allowedJobs is not empty', () => {
+            describe('and the job is in allowedJobs', async () => {
+              it('returns the triggered job', async () => {
+                const result = await handler.handle(event, ['the_job']);
+
+                expect(result).toEqual('Trigger: the_job, Branch: fix');
+              });
             });
+
+            describe('and the job is not in allowedJobs', async () => {
+              it('returns the triggered job', async () => {
+                const result = await handler.handle(event, ['not_the_job']);
+
+                expect(result).toEqual('Not allowed: the_job');
+              });
+            });
+          });
+        });
+
+        describe('and it failed to trigger the job', () => {
+          beforeEach(() => {
+            moxios.stubRequest(
+              'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
+              {
+                status: 403,
+                response: { message: 'Forbidden' },
+              },
+            );
+            moxios.stubRequest(
+              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              { status: 201, response: {} },
+            );
+          });
+
+          it('throws an error', async () => {
+            const err = await handler.handle(event, []).catch(err => err);
+
+            expect(err.message).toEqual('Request failed with status code 403');
           });
         });
       });
 
-      describe('and it failed to trigger the job', () => {
+      describe('help command', () => {
         beforeEach(() => {
-          moxios.stubRequest(
-            'https://circleci.com/api/v1.1/project/github/yuya-takeyama/gh-circle-trigger-proto/tree/fix',
-            {
-              status: 403,
-              response: { message: 'Forbidden' },
-            },
-          );
-          moxios.stubRequest(
-            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            { status: 201, response: {} },
-          );
+          event.payload.comment.body = '@bot help';
         });
 
-        it('throws an error', async () => {
-          const err = await handler.handle(event, []).catch(err => err);
+        it('returns "Help"', async () => {
+          moxios.stubRequest(event.payload.issue.pull_request.comments_url, {
+            status: 201,
+            response: {},
+          });
+          const result = await handler.handle(event, []);
 
-          expect(err.message).toEqual('Request failed with status code 403');
+          expect(result).toEqual('Help');
         });
       });
     });
 
-    describe('when it does not contain trigger word', async () => {
-      const result = await handler.handle(event, []);
+    describe('when it does not contain trigger word', () => {
+      it('returns NOOP', async () => {
+        const result = await handler.handle(event, []);
 
-      expect(result).toEqual('NOOP');
+        expect(result).toEqual('NOOP');
+      });
     });
   });
 
--- src/commands.test.ts
+++ src/commands.test.ts
@@ -1,8 +1,17 @@
 import moxios from 'moxios';
-import { CommandParser, NoopCommand, TriggerCommand } from './commands';
+import {
+  CommandParser,
+  HelpCommand,
+  NoopCommand,
+  TriggerCommand,
+} from './commands';
 import { Config } from './config';
 import Github from './Github';
-import { IssueCommentEvent, PullRequestEvent } from './interfaces/github';
+import {
+  IssueCommentEvent,
+  PullRequestEntity,
+  PullRequestEvent,
+} from './interfaces/github';
 
 describe('Commandparser', () => {
   const config: Config = {
@@ -23,72 +32,12 @@ describe('Commandparser', () => {
 
   describe('#parse', () => {
     describe('with PullRequestEvent', () => {
-      const event: PullRequestEvent = {
-        event: 'pull_request',
-        payload: {
-          action: 'opened',
-          pull_request: {
-            url:
-              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
-            id: 172665130,
-            body: 'Hello',
-            comments_url:
-              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-            head: {
-              ref: 'fix',
-            },
-            base: {
-              ref: 'master',
-            },
-          },
-          repository: {
-            full_name: 'yuya-takeyama/gh-circle-trigger-proto',
-          },
-        },
-      };
-
-      describe('without job name', () => {
-        it('returns NoopCommand', async () => {
-          const command = await commandParser.parse(event);
-          const expected: NoopCommand = { type: 'noop' };
-          expect(command).toEqual(expected);
-        });
-      });
-
-      describe('without job name', () => {
-        it('returns TriggerCommand', async () => {
-          event.payload.pull_request.body = '@triggerbot trigger build';
-          const command = await commandParser.parse(event);
-          const expected: TriggerCommand = {
-            type: 'trigger',
-            branch: 'fix',
-            repository: 'yuya-takeyama/gh-circle-trigger-proto',
-            pullRequest: event.payload.pull_request,
-            job: 'build',
-          };
-          expect(command).toEqual(expected);
-        });
-      });
-
-      describe('action is not "opened"', () => {
-        it('returns NoopCommand', async () => {
-          event.payload.action = 'edited';
-          const command = await commandParser.parse(event);
-          expect(command).toEqual({ type: 'noop' });
-        });
-      });
-    });
-
-    describe('with IssueCommentEvent', () => {
-      const event: IssueCommentEvent = {
-        event: 'issue_comment',
-        payload: {
-          action: 'created',
-          issue: {
-            url:
-              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2',
-            comments_url:
-              'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+      let event: PullRequestEvent;
+      beforeEach(() => {
+        event = {
+          event: 'pull_request',
+          payload: {
+            action: 'opened',
             pull_request: {
               url:
                 'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
@@ -103,66 +52,158 @@ describe('Commandparser', () => {
                 ref: 'master',
               },
             },
+            repository: {
+              full_name: 'yuya-takeyama/gh-circle-trigger-proto',
+            },
           },
-          comment: {
-            body: 'Hello',
-          },
-          repository: {
-            full_name: 'yuya-takeyama/gh-circle-trigger-proto',
-          },
-        },
-      };
+        };
+      });
 
-      const pullRequest = {
-        url:
-          'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
-        id: 172665130,
-        body: 'Hello',
-        comments_url:
-          'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
-        head: {
-          ref: 'fix',
-        },
-        base: {
-          ref: 'master',
-        },
-      };
+      describe('trigger command', () => {
+        describe('without job name', () => {
+          it('returns NoopCommand', async () => {
+            const command = await commandParser.parse(event);
+            const expected: NoopCommand = { type: 'noop' };
+            expect(command).toEqual(expected);
+          });
+        });
+
+        describe('without job name', () => {
+          it('returns TriggerCommand', async () => {
+            event.payload.pull_request.body = '@triggerbot trigger build';
+            const command = await commandParser.parse(event);
+            const expected: TriggerCommand = {
+              type: 'trigger',
+              branch: 'fix',
+              repository: 'yuya-takeyama/gh-circle-trigger-proto',
+              pullRequest: event.payload.pull_request,
+              job: 'build',
+            };
+            expect(command).toEqual(expected);
+          });
+        });
+
+        describe('action is not "opened"', () => {
+          it('returns NoopCommand', async () => {
+            event.payload.action = 'edited';
+            const command = await commandParser.parse(event);
+            expect(command).toEqual({ type: 'noop' });
+          });
+        });
+      });
+
+      describe('help command', () => {
+        it('returns NoopCommand', async () => {
+          event.payload.pull_request.body = '@triggerbot help';
+          const command = await commandParser.parse(event);
+          const expected: HelpCommand = {
+            type: 'help',
+            pullRequest: event.payload.pull_request,
+          };
+          expect(command).toEqual(expected);
+        });
+      });
+    });
+
+    describe('with IssueCommentEvent', () => {
+      let event: IssueCommentEvent;
+      let pullRequest: PullRequestEntity;
 
       beforeEach(() => {
+        event = {
+          event: 'issue_comment',
+          payload: {
+            action: 'created',
+            issue: {
+              url:
+                'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2',
+              comments_url:
+                'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+              pull_request: {
+                url:
+                  'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
+                id: 172665130,
+                body: 'Hello',
+                comments_url:
+                  'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+                head: {
+                  ref: 'fix',
+                },
+                base: {
+                  ref: 'master',
+                },
+              },
+            },
+            comment: {
+              body: 'Hello',
+            },
+            repository: {
+              full_name: 'yuya-takeyama/gh-circle-trigger-proto',
+            },
+          },
+        };
+        pullRequest = {
+          url:
+            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/pulls/2',
+          id: 172665130,
+          body: 'Hello',
+          comments_url:
+            'https://api.github.com/repos/yuya-takeyama/gh-circle-trigger-proto/issues/2/comments',
+          head: {
+            ref: 'fix',
+          },
+          base: {
+            ref: 'master',
+          },
+        };
         moxios.stubRequest(event.payload.issue.pull_request.url, {
           status: 200,
           response: pullRequest,
         });
       });
 
-      describe('without job name', () => {
-        it('returns NoopCommand', async () => {
-          const command = await commandParser.parse(event);
-          const expected: NoopCommand = { type: 'noop' };
-          expect(command).toEqual(expected);
+      describe('trigger command', () => {
+        describe('without job name', () => {
+          it('returns NoopCommand', async () => {
+            const command = await commandParser.parse(event);
+            const expected: NoopCommand = { type: 'noop' };
+            expect(command).toEqual(expected);
+          });
+        });
+
+        describe('with job name', () => {
+          it('returns TriggerCommand', async () => {
+            event.payload.comment.body = '@triggerbot trigger build';
+            const command = await commandParser.parse(event);
+            const expected: TriggerCommand = {
+              pullRequest,
+              type: 'trigger',
+              branch: 'fix',
+              repository: 'yuya-takeyama/gh-circle-trigger-proto',
+              job: 'build',
+            };
+            expect(command).toEqual(expected);
+          });
+        });
+
+        describe('when action is not "created"', () => {
+          it('returns NoopCommand', async () => {
+            event.payload.action = 'edited';
+            const command = await commandParser.parse(event);
+            const expected: NoopCommand = { type: 'noop' };
+            expect(command).toEqual(expected);
+          });
         });
       });
 
-      describe('with job name', () => {
-        it('returns TriggerCommand', async () => {
-          event.payload.comment.body = '@triggerbot trigger build';
+      describe('help command', () => {
+        it('returns HelpCommand', async () => {
+          event.payload.comment.body = '@triggerbot help';
           const command = await commandParser.parse(event);
-          const expected: TriggerCommand = {
-            pullRequest,
-            type: 'trigger',
-            branch: 'fix',
-            repository: 'yuya-takeyama/gh-circle-trigger-proto',
-            job: 'build',
+          const expected: HelpCommand = {
+            type: 'help',
+            pullRequest: event.payload.issue.pull_request,
           };
-          expect(command).toEqual(expected);
-        });
-      });
-
-      describe('when action is not "created"', () => {
-        it('returns NoopCommand', async () => {
-          event.payload.action = 'edited';
-          const command = await commandParser.parse(event);
-          const expected: NoopCommand = { type: 'noop' };
           expect(command).toEqual(expected);
         });
       });
